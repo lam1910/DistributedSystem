585fa956cb5b3f20e08479a1a263210849c76d35
lam1910 <nguengoclam19@gmail.com>
2020-04-03 00:23:32 +0700

    week2 homework

---
+++ JMSTopicProject/src/com/lamnn/MyListener.java
@@ -0,0 +1,16 @@
+package com.lamnn;
+
+import javax.jms.*;
+
+public class MyListener implements MessageListener {
+
+	public void onMessage(Message m) {
+		try {
+			TextMessage msg = (TextMessage) m;
+			System.out.println("following message is received:" + msg.getText());
+		} catch (JMSException e) {
+			System.out.println(e);
+		}
+	}
+
+}
